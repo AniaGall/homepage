34c67fc8c5151b651719d57aa1e243f92ab8d639
Anna Gaładyk <anngaladyk@gmail.com>
2020-05-05 22:54:14 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstName = 'Ania';
+const age = 37;
+
+console.log (firstName);
+console.log (age);
+
+alert ('Siema!')
+console.log('Witaj WTF!');
+console.log('Co słychać?');
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`)
