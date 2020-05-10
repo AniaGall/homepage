b0df3259e0f893c598c8b41d8d5b172dcb3f23e4
Anna Gaładyk <anngaladyk@gmail.com>
2020-05-11 00:43:31 +0200

    DOM in jd

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ alert("Siema!");
 console.log("Witaj WTF!");
 console.log("Co słychać?");
 console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
+
+const header = document.querySelector('.new-header--js');
+
+console.log(header);
+
+header.innerHTML = 'Ania Gaładyk - szkolenie WTF'
